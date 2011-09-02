84374c2c38c9daf71fc008a59a687390dc707d96
Ben <benbenben001@gmail.com>
2011-09-02 22:42:44 +0100

    Updated to 1.3.2
    A few fixes

--- src/de/bananaco/permissions/override/MonkeyPlayer.java
+++ src/de/bananaco/permissions/override/MonkeyPlayer.java
@@ -20,6 +20,7 @@ public class MonkeyPlayer extends CraftPlayer {
 	public boolean hasPermission(String name) {
 		boolean has = isOp();
 		try {
+			
 			has = internalHasPermission(name);
 		} catch (Exception e) {
 			System.err.println("[bPermissions] Something horrible went wrong. Please turn off your computer and walk away.");
@@ -29,14 +30,14 @@ public class MonkeyPlayer extends CraftPlayer {
 	
     private boolean internalHasPermission(String name) {
     	PermissionSet p = Permissions.getWorldPermissionsManager().getPermissionSet(getWorld());
-        if (p.has(this, name))
+        if (isPermissionSet(name))
             return p.has(this, name);
-        
+        System.out.println("test");
         int index = name.lastIndexOf('.');
         while (index >= 0) {
             name = name.substring(0, index);
             String wildcard = name + ".*";
-            if (p.has(this, wildcard))
+            if (isPermissionSet(wildcard))
                 return p.has(this, wildcard);
             index = name.lastIndexOf('.');
         }
