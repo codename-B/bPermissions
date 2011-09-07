f58bb57f370cafceedd0c35fe4964aa0915f5cfa
Ben <benbenben001@gmail.com>
2011-09-07 14:44:26 +0100

    PlayerInfo.java
    YUNOWURK D:

---
+++ src/com/ubempire/binfo/PlayerInfo.java
@@ -0,0 +1,12 @@
+package com.ubempire.binfo;
+
+import org.bukkit.entity.Player;
+
+public interface PlayerInfo {
+    
+    public String getPrefix(Player player);
+    
+    public String getSuffix(Player player);
+    
+}
+
