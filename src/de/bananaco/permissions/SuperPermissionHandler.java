c3b9fc0e235b25b206034d8029e5375571e2dd56
Ben <benbenben001@gmail.com>
2011-10-26 20:27:29 +0100

    More fixes

--- src/de/bananaco/permissions/SuperPermissionHandler.java
+++ src/de/bananaco/permissions/SuperPermissionHandler.java
@@ -1,7 +1,6 @@
 package de.bananaco.permissions;
 
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 import org.bukkit.entity.Player;
@@ -16,19 +15,15 @@ public class SuperPermissionHandler {
 	public static void setupPlayer(Player p, List<String> nodes, JavaPlugin plugin) {
 		long start = System.currentTimeMillis();
 		unsetupPlayer(p, plugin);
-		/*
-		 * Test fix for mChat - dunno wtf is up
-		 */
+		PermissionAttachment att = p.addAttachment(plugin);
 		for (String node : nodes) {
 			String tNode = (node.startsWith("^")?node.replace("^",""):node);
-		PermissionAttachment att = p.addAttachment(plugin);
 			if (node.startsWith("^")) {
 				att.setPermission(tNode, false);
 			} else {
 				att.setPermission(tNode, true);
 			}
 		}
-		
 		long finish = System.currentTimeMillis()-start;
 		Debugger.getDebugger().log("Setup player:"+p.getName()+" took "+finish+"ms");
 	}
@@ -37,19 +32,15 @@ public class SuperPermissionHandler {
 		Set<PermissionAttachmentInfo> pAtt = p.getEffectivePermissions();
 		if (pAtt != null) {
 			for (PermissionAttachmentInfo pInfo : pAtt) {
-				PermissionAttachment pa = pInfo.getAttachment();
-				if(pa != null) {
-				Map<String, Boolean> paNodes = pa.getPermissions();
-				if (paNodes != null) {
-					Set<String> keys = paNodes.keySet();
-					for (String key : keys) {
-						pa.unsetPermission(key);
-					}
+				Debugger.getDebugger().log(pInfo.getPermission());
+				if(pInfo.getAttachment() != null) {
+				pInfo.getAttachment().unsetPermission(pInfo.getPermission());
+				pInfo.getAttachment().remove();
 				}
 			}
-			}
-			p.recalculatePermissions();
 		}
+		p.recalculatePermissions();
 	}
 
 }
+
