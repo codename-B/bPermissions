12aacc5bef10f0933e99d507665e5fc4b20215f6
Santtu Elo <iffamies@hotmail.fi>
2011-11-05 20:55:27 +0200

    Oops!

--- src/com/arandomappdev/bukkitstats/CallHome.java
+++ src/com/arandomappdev/bukkitstats/CallHome.java
@@ -24,7 +24,7 @@
 * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
 * OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
-package com.randomappdev.bukkitstats;
+package com.arandomappdev.bukkitstats;
  
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.YamlConfiguration;
