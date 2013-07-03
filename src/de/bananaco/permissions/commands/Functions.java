61129d2a41a72bcae3b2b6b5ed1c838592bab720
Ben <benbenben001@gmail.com>
2013-07-03 18:22:22 +0100

    more command work

--- src/de/bananaco/permissions/commands/Functions.java
+++ src/de/bananaco/permissions/commands/Functions.java
@@ -33,6 +33,7 @@ public class Functions implements CommandExecutor {
         ADD(new String[] {"ad", "pl", "add"}),
         REMOVE(new String[] {"rm", "remo", "remove", "remov"}),
         SET(new String[] {"s", "se", "set"}),
+        LIST(new String[] {"l", "li", "lis", "list", "ls", "lst"}),
         NULL(new String[0]);
 
         private final String[] aliases;
@@ -53,17 +54,23 @@ public class Functions implements CommandExecutor {
 
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if(ApiLayer.isGlobal()) {
-            if(args.length > 3) {
-                return error(sender, FunctionType.NULL, ActionType.NULL);
+            if(args.length < 2) {
+                FunctionType type = args.length >= 1?FunctionType.getType(args[0]):FunctionType.NULL;
+                ActionType action = args.length >= 2?ActionType.getType(args[1]):ActionType.NULL;
+                return error(sender, type, action);
             } else {
                 FunctionType type = FunctionType.getType(args[0]);
                 ActionType action = ActionType.getType(args[1]);
-                String value = args.length>=3?args[3]:null;
-                String data = args.length>=4?args[4]:null;
+                String value = args.length>=3?args[2]:null;
+                String data = args.length>=4?args[3]:null;
                 return executeGlobal(sender, type, action, value, data);
             }
         } else {
-            if(args.length > 4) {
+            if(args.length < 3) {
+                FunctionType type = args.length >= 2?FunctionType.getType(args[1]):FunctionType.NULL;
+                ActionType action = args.length >= 3?ActionType.getType(args[2]):ActionType.NULL;
+                return error(sender, type, action);
+            } else {
                 String world = args[0];
                 FunctionType type = FunctionType.getType(args[1]);
                 ActionType action = ActionType.getType(args[2]);
@@ -72,23 +79,54 @@ public class Functions implements CommandExecutor {
                 return executeWorld(sender, world, type, action, value, data);
             }
         }
-        return true;
     }
 
     public boolean error(CommandSender sender, FunctionType type, ActionType action) {
-        sender.sendMessage(ChatColor.RED+"Sorry, "+ChatColor.AQUA+action.name().toLowerCase()+ChatColor.RED+" is not valid for "+ChatColor.AQUA+type.name().toLowerCase());
+        sender.sendMessage(ChatColor.RED+"Sorry, "+ChatColor.AQUA+action.name().toLowerCase()+ChatColor.RED+" is not valid for "+ChatColor.AQUA+type.name().toLowerCase()+ChatColor.RED+" in the command you attempted.");
         return true;
     }
 
     public boolean executeGlobal(CommandSender sender, FunctionType type, ActionType action, String value, String data) {
         // error messages for letting people know about stuff
         if(type.equals(FunctionType.PACKAGE)) {
-            return error(sender, type, action);
+            if(value == null) {
+                if(action.equals(ActionType.LIST)) {
+
+                }
+                sender.sendMessage(ChatColor.RED+"Please specify a package");
+                return true;
+            }
+            if(action.equals(ActionType.LIST)) {
+
+            }
+            if(data == null) {
+                sender.sendMessage(ChatColor.RED+"Please specify a permission to add");
+                return true;
+            }
+            if(action.equals(ActionType.ADD)) {
+
+            } else if(action.equals(ActionType.REMOVE)) {
+
+            }
         }
         if(type.equals(FunctionType.PLAYER)) {
-            return error(sender, type, action);
+            if(value == null) {
+                if(action.equals(ActionType.LIST)) {
+
+                }
+                sender.sendMessage(ChatColor.RED+"Please specify a player");
+                return true;
+            }
+            if(action.equals(ActionType.LIST)) {
+
+            }
+            if(data == null) {
+                sender.sendMessage(ChatColor.RED+"Please specify a package");
+                return true;
+            }
+
         }
-        return true;
+        return error(sender, type, action);
     }
 
     public boolean executeWorld(CommandSender sender, String world, FunctionType type, ActionType action, String value, String data) {
